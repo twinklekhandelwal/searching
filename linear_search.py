1bfbd3dd110bed7c441c9ce91aeac2f26c849cb9
twinkle <you@example.com>
2019-06-11 11:24:24 +0530

    user input

--- linear_search.py
+++ linear_search.py
@@ -1,6 +1,8 @@
 linear_list=[2,3,9,76,54,65,45,67]
 i=0
+user_input=int(raw_input("enter no."))
 while(i<len(linear_list)):
-	if(linear_list[i]==76):
+	if(linear_list[i]==user_input):
 	    print linear_list[i]
+        print i
 	i=i+1    
